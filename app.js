4a8f42c88d463a846d274fba273eacce03b5fc8f
Adam Robson <adamray312@gmail.com>
2022-08-25 09:15:17 -0700

    'minimal additions'

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import { checkAuth, signOutUser } from './fetch-utils.js';
 
 /*  "boiler plate" auth code */
 // checking if we have a user! (will redirect to auth if not):
-checkAuth();
 // can optionally return the user:
+checkAuth();
 // const user = checkAuth();
 
 // sign out link:
@@ -15,7 +15,13 @@ signOutLink.addEventListener('click', signOutUser);
 /* end "boiler plate auth code" */
 
 // grab needed DOM elements on page:
-
+// const chilisForm = document.getElementById('chilis-form');
+// const chilisNameInput = document.getElementById('chilis-name');
+// const chilisFileUpload = document.getElementById('chilis-file-upload');
+// const chilisImage = document.getElementById('chilis-image');
+// const chilisRatingTextarea = document.getElementById('chilis-rating-textarea');
+// const addCommentButton = document.getElementById('add-chilis-comment');
+// const imageContainer = document.getElementById('image-container');
 // local state:
 
 // display functions:
